11b855a5cdde6d3fd9b1b77d38b285c37a873fa1
root <root@pve.tds>
2025-04-02 20:39:07 -0700

    Reset git index to match .gitignore rules

---
+++ Week1/Git-Assignment/Chris_Chausse.py
@@ -0,0 +1,14 @@
+# Chris Chausse
+# Date 3/31/2025
+# Week 1 Assignment: Writing Functions as a Group/Team
+
+def rect_area(length, width):
+    return length * width
+    
+    
+def rect_surface_area(length, width, height):
+    front_back = 2 * rect_area(length, width)
+    top_bottom = 2 * rect_area(length, height)
+    sides = 2 * rect_area(width, height)
+    
+    return (front_back + top_bottom + sides)

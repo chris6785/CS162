398a471526529a1d38d47b02d88d693c94e6d093
Chris <chris_chausse@hotmail.com>
2025-04-05 23:32:06 -0700

    Word doc and photo Commit

--- Week1/Git-Assignment/Chris_Chausse.py
+++ Week1/Git-Assignment/Chris_Chausse.py
@@ -1,11 +1,12 @@
+from will import *
 # Chris Chausse
 # Date 3/31/2025
 # Week 1 Assignment: Writing Functions as a Group/Team
 
-def rect_area(length, width):
-    return length * width
+#def rect_area(length, width):
+#    return length * width
     
-    
+# Written by Chris Chausse    
 def rect_surface_area(length, width, height):
     front_back = 2 * rect_area(length, width)
     top_bottom = 2 * rect_area(length, height)
